0ba413d2c9b62c50807bc830c1f1d068f7196770
zhravan <mrshravankumarb@gmail.com>
2026-03-04 05:00:56 +0530

    Add CLI skeleton for run, restore, status

--- cmd/silo/main.go
+++ cmd/silo/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	os.Exit(0)
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+	switch os.Args[1] {
+	case "run":
+		// TODO: run backup
+		os.Exit(0)
+	case "restore":
+		// TODO: restore from backup
+		os.Exit(0)
+	case "status":
+		// TODO: show status
+		os.Exit(0)
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, "Usage: silo <command>\n")
+	fmt.Fprintf(os.Stderr, "Commands: run, restore, status\n")
 }
